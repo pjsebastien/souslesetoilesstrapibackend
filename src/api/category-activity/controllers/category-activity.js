6efaad2549fd60f64148c5043e75428b1a5c188e
pierrejean <pj.sebastien@gmail.com>
2022-07-21 08:51:15 +0400

    change activity

---
+++ src/api/category-activity/controllers/category-activity.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  category-activity controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::category-activity.category-activity');
---
+++ src/api/category-activity/routes/category-activity.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * category-activity router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::category-activity.category-activity');
